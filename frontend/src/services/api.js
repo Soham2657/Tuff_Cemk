625357ff78a23a525208fb3eeae73871a462133f
Soham Bhattacharyya <cse23100@cemk.ac.in>
2026-04-30 11:24:43 +0530

    backend url push

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Pointing to local backend
+  baseURL: process.env.backend_url, // Pointing to deployed backend
 });
 
 // Add a request interceptor to inject the token
